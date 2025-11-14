ec3452a50db0fca6df1cba94bdea7df80832119c
arcenlupin519 <arcenlupin519@outlook.com>
2025-11-14 18:35:40 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,25 +1,34 @@
-project = 'trend micro login'
-author = 'trend micro login'
+project = 'Trend Micro Login'
+author = 'Trend Micro Login'
 release = '1.0'
 
-extensions = []
-templates_path = ['_templates']
-exclude_patterns = []
-
-html_theme = 'alabaster'
-html_static_path = ['_static']   
-
-html_js_files = [
-    'chatbot.js',
-]
-html_favicon = '_static/favicon.png'
-
-html_context = {
-    'bing_verification_code': '739245F5D54BCBF40AC056DC0CBF5710'
-}
-
+# Sphinx Extensions
 extensions = [
     'sphinx_sitemap',
 ]
 
+# Templates & Patterns
+templates_path = ['_templates']
+exclude_patterns = []
+
+# ReadTheDocs Friendly Theme (Highly Recommended)
+html_theme = 'sphinx_rtd_theme'
+
+# Static Files
+html_static_path = ['_static']
+
+# Optional JS script
+html_js_files = [
+    'chatbot.js',
+]
+
+# Favicon
+html_favicon = '_static/favicon.png'
+
+# Important for Sitemap
 html_baseurl = 'https://microtrendinfoguide.readthedocs.io/en/latest/'
+
+# Bing Verification
+html_context = {
+    'bing_verification_code': '739245F5D54BCBF40AC056DC0CBF5710'
+}
